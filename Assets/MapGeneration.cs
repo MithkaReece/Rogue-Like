b5fd6c0868e655ae457ca15c45c574f1ff304594
MithkaReece <reece.waite1@gmail.com>
2023-06-21 20:50:02 +0100

    Fixed ordering
    -Fixed so ground always appears behind player
    -Collision objects appear behind player sometimes but as not inside its still works fine

--- Assets/MapGeneration.cs
+++ Assets/MapGeneration.cs
@@ -6,28 +6,34 @@ using UnityEngine.Tilemaps;
 public class MapGeneration : MonoBehaviour
 {
 
-    private Tilemap tilemap;
+    private Tilemap ground;
+    private Tilemap collisions;
 
     void Awake()
     {
-        tilemap = GetComponent<Tilemap>();
+        ground = transform.GetChild(0).GetComponent<Tilemap>();
+        collisions = transform.GetChild(1).GetComponent<Tilemap>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        RuleTile outside = Resources.Load<RuleTile>("Tiles/TilePalettes/Prison/OutsideRuleTile");
-        Setup(new Vector3Int(0, 0, 0), 10, 10, outside);
+        RuleTile floor = Resources.Load<RuleTile>("Tiles/Ground/OutsideRuleTile");
+        TileBase edges = Resources.Load<TileBase>("Tiles/Collisions/Black");
+
+        rectangle(new Vector3Int(-5, -5, 0), 10, 10, floor, edges);
     }
 
-    void Setup(Vector3Int corner, int width, int height, TileBase tile)
+    void rectangle(Vector3Int corner, int width, int height, TileBase floor, TileBase edges)
     {
         for (int x = corner.x; x < corner.x + width; x++)
         {
             for (int y = corner.y; y < corner.y + height; y++)
             {
-                Vector3Int tilePosition = new Vector3Int(x, y, corner.z);
-                tilemap.SetTile(tilePosition, tile);
+                if (x == corner.x || x == corner.x+width-1 || y == corner.y || y == corner.y + height - 1)
+                    collisions.SetTile(new Vector3Int(x, y, corner.z), edges);
+                else
+                    ground.SetTile(new Vector3Int(x, y, corner.z), floor);
             }
         }
     }
